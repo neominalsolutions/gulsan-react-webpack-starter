f300557efd2e9f7a2af0858a41f463cbae24ad6f
neominal.solutions <neominal.solutions@gmail.com>
2024-07-25 14:47:27 +0300

    React WEBPack Full Proje Template

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import aboutImage from '../src/assets/images/about-us.png';
 import jsonData from '../src/assets/json/demo.json';
 
 import './index.scss';
+import CssModuleDemo from './components/css-modules/css-module.demo';
 
 function App() {
 	return (
 		<>
 			<h1 className="App">Uygulamanın Ana giriş Noktası</h1>
 			<img width={100} height={100} src={aboutImage} />
+
+			<p className="text-lg underline font-bold text-blue-500">Tailwind CSS</p>
+
+			<CssModuleDemo />
 		</>
 	);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
 			},
 			{
 				test: /\.(s(a|c)ss)$/,
-				use: ['style-loader', 'css-loader', 'sass-loader'],
+				use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
 			},
 			{
 				test: /\.(woff|woff2|eot|ttf|svg)$/,
