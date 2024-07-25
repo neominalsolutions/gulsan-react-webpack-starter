28685891f1e5dbb99b212633d866e7236b1f24ca
neominal.solutions <neominal.solutions@gmail.com>
2024-07-25 18:59:14 +0300

    Prime React Tailwindcss Demo

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,45 @@
-import React from 'react';
-
-import aboutImage from '../src/assets/images/about-us.png';
-import jsonData from '../src/assets/json/demo.json';
+import { PrimeIcons, PrimeReactProvider } from 'primereact/api';
+import Tailwind from 'primereact/passthrough/tailwind';
+import React, { useCallback } from 'react';
 
 import './index.scss';
-import CssModuleDemo from './components/css-modules/css-module.demo';
+import { Menu } from 'primereact/menu';
+import { Button } from 'primereact/button';
+
+function PrimeDemo() {
+	const click = useCallback(() => {
+		alert('click');
+	}, []);
+
+	const items = [
+		{
+			label: 'File',
+			items: [
+				{ label: 'New', icon: PrimeIcons.PLUS },
+				{ label: 'Open', icon: PrimeIcons.DOWNLOAD },
+			],
+		},
+	];
+
+	return (
+		<>
+			<h1 className="underline font-bold text-2xl text-red-500 mb-2">
+				Prime React With Tailwind
+			</h1>
+			<Menu className="mb-2" model={items} />
+			<Button icon={'pi pi-plus'} onClick={click} label="Submit" />
+		</>
+	);
+}
 
 function App() {
 	return (
 		<>
-			<h1 className="App">Uygulamanın Ana giriş Noktası</h1>
-			<img width={100} height={100} src={aboutImage} />
-
-			<p className="text-lg underline font-bold text-blue-500">Tailwind CSS</p>
-
-			<CssModuleDemo />
+			<PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+				<div className="p-5">
+					<PrimeDemo />
+				</div>
+			</PrimeReactProvider>
 		</>
 	);
 }
